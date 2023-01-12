cec59cb4b7820c43a4f69b8bdfc75b2423ca64be
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2023-01-12 14:33:19 -0700

    Solved all exercises

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -27,6 +27,44 @@ module Enumerable
 
     true
   end
+
+  def my_any?(&block)
+    for el in self do
+      return true if block.call el
+    end
+    false
+  end
+
+  def my_none?(&block)
+    !my_any?(&block)
+  end
+
+  def my_count(&block)
+    counter = 0
+    if block_given?
+      for el in self do
+        counter += 1 if block.call el
+      end
+      counter
+    else
+      length
+    end
+  end
+
+  def my_map(&block)
+    arr = []
+    for el in self do
+      arr.push(block.call(el))
+    end
+    arr
+  end
+
+  def my_inject(init = 0, &block)
+    for el in self do
+      init = block.call init, el
+    end
+    init
+  end
 end
 
 # You will first have to define my_each
